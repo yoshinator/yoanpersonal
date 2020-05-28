273f077283b33e10a67f073f783af00ad43856b5
yoshinator <yoanante@gmail.com>
2020-05-27 23:07:29 -0400

    trying to serve up react

---
+++ memory/precache-manifest.40e531bc6d1967fed051e06c72be96b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86743a5319748988a69d",
+    "url": "/memory-castle-front-end/static/css/main.82ae191d.chunk.css"
+  },
+  {
+    "revision": "86743a5319748988a69d",
+    "url": "/memory-castle-front-end/static/js/main.86743a53.chunk.js"
+  },
+  {
+    "revision": "7f0b306d0a2b45839998",
+    "url": "/memory-castle-front-end/static/js/1.7f0b306d.chunk.js"
+  },
+  {
+    "revision": "60f0b03ab4630afcac19",
+    "url": "/memory-castle-front-end/static/js/runtime~main.60f0b03a.js"
+  },
+  {
+    "revision": "25497bf1650b1ca0403468a03890a0ab",
+    "url": "/memory-castle-front-end/static/media/bg-bricks.25497bf1.jpg"
+  },
+  {
+    "revision": "0399ab68d1cff36025b56efacadd8790",
+    "url": "/memory-castle-front-end/static/media/SansForgetica-Regular.0399ab68.otf"
+  },
+  {
+    "revision": "2b6ad2f936d3eec8f1f88e024ee2068f",
+    "url": "/memory-castle-front-end/index.html"
+  }
+];
